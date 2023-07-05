f5a9aa67f710386b712c4db47a67cd819f1d51f6
ardraivan <ardraivand@gmail.com>
2023-07-05 14:06:21 +0700

    proyek template dashboard

---
+++ dashboard/resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,25 @@
+<!-- Navbar -->
+<nav style="background-color: #F1F2F6" class="main-header navbar navbar-expand ">
+    <!-- Left navbar links -->
+    <ul id="listmenuheader" class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i
+                    class="fas fa-bars"></i></a>
+        </li>
+        <li class="nav-item d-none d-sm-inline-block">
+            <a href="index3.html" class="nav-link">Home</a>
+        </li>
+
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                <i style="color: #34333A" class="fas fa-expand-arrows-alt"></i>
+            </a>
+        </li>
+
+    </ul>
+</nav>
+<!-- /.navbar -->
