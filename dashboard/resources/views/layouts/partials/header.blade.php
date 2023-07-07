f93f83f2f1d621d187a6e10d5a64783985524418
dandy.alv <dandy.alv@gmail.com>
2023-07-07 23:47:54 +0800

    hapus tulisan home di header

--- dashboard/resources/views/layouts/partials/header.blade.php
+++ dashboard/resources/views/layouts/partials/header.blade.php
@@ -6,9 +6,6 @@
                 <i class="fas fa-bars"></i>
             </a>
         </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="index3.html" class="nav-link">Home</a>
-        </li>
     </ul>
 
     <!-- Right navbar links -->
